50a836830a140074ce22f33b6aa0fc53fbcd575a
charlesCaballero <charles.g.caballero@gmail.com>
2025-03-07 16:55:58 +0800

    changed help icon

--- resources/js/Components/Forms/BoxForm/icons.tsx
+++ resources/js/Components/Forms/BoxForm/icons.tsx
@@ -27,7 +27,14 @@ export const TrashIcon = () => {
 
 export const HelpIcon = () => {
     return (
-        <svg width={20} height={20} viewBox="0 0 16 16" fill="currentColor">
+        <svg
+            width={24}
+            height={24}
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            className="text-foreground/40"
+        >
             <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
             <g
                 id="SVGRepo_tracerCarrier"
@@ -35,11 +42,7 @@ export const HelpIcon = () => {
                 strokeLinejoin="round"
             ></g>
             <g id="SVGRepo_iconCarrier">
-                {" "}
-                <path
-                    d="m 8 0 c -4.410156 0 -8 3.589844 -8 8 s 3.589844 8 8 8 s 8 -3.589844 8 -8 s -3.589844 -8 -8 -8 z m 0 2 c 3.332031 0 6 2.667969 6 6 s -2.667969 6 -6 6 s -6 -2.667969 -6 -6 s 2.667969 -6 6 -6 z m 0 1.875 c -0.621094 0 -1.125 0.503906 -1.125 1.125 s 0.503906 1.125 1.125 1.125 s 1.125 -0.503906 1.125 -1.125 s -0.503906 -1.125 -1.125 -1.125 z m -1.523438 3.125 c -0.265624 0.011719 -0.476562 0.230469 -0.476562 0.5 c 0 0.277344 0.222656 0.5 0.5 0.5 h 0.5 v 3 h -0.5 c -0.277344 0 -0.5 0.222656 -0.5 0.5 s 0.222656 0.5 0.5 0.5 h 3 c 0.277344 0 0.5 -0.222656 0.5 -0.5 s -0.222656 -0.5 -0.5 -0.5 h -0.5 v -4 h -2.5 c -0.007812 0 -0.015625 0 -0.023438 0 z m 0 0"
-                    fill="currentColor"
-                ></path>{" "}
+                <path d="M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z"></path>
             </g>
         </svg>
     );
