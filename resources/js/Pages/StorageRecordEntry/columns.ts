fa91f91285f4518ca5ac9dafeec3b7f7b07e6f83
charlesCaballero <charles.g.caballero@gmail.com>
2025-03-19 16:50:29 +0800

    fixed some bugs and priority level is changed to automatic update.

--- resources/js/Pages/StorageRecordEntry/columns.ts
+++ resources/js/Pages/StorageRecordEntry/columns.ts
@@ -1,7 +1,8 @@
 export const columns = [
     { name: "BOX CODE", uid: "box_code" },
-    { name: "DISPOSAL DATE", uid: "disposal_date" },
+    { name: "PRIORITY LEVEL", uid: "priority_level" },
     { name: "OFFICE", uid: "office" },
+    { name: "DISPOSAL DATE", uid: "disposal_date" },
     { name: "REMARKS", uid: "remarks" },
     { name: "BOX CONTENTS", uid: "documents" },
     { name: "ACTIONS", uid: "actions" },
